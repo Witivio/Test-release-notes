169e00e4826f1f00a42734e36ef6bf1485f08189
Gabrielle <gmarsallon@witivio.com>
2024-01-23 11:01:23 +0100

    script & job

---
+++ scripts/update_documentation_script_git.ps1
@@ -0,0 +1,14 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$FilePath,
+    [Parameter(Mandatory=$true)]
+    [string]$MarkdownData
+)
+
+$title = "## What's new on GPTPro ?"
+
+$contentToWrite = "$title`r`n$MarkdownData"
+
+$contentToWrite | Set-Content -Path $FilePath -Force
+
+Write-Output "Markdown data has been written to $FilePath"
