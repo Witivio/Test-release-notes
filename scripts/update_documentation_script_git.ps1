df6716823b8348ae7a764316268f8095d3e2913d
Gabrielle <gmarsallon@witivio.com>
2024-01-23 12:36:54 +0100

    debug

--- scripts/update_documentation_script_git.ps1
+++ scripts/update_documentation_script_git.ps1
@@ -7,7 +7,11 @@ param (
 
 $title = "## What's new on GPTPro ?"
 
+Write-Output "Debug :  $MarkdownData"
+
 $contentToWrite = "$title`r`n$MarkdownData"
+
+Write-Output "Debug2 :  $contentToWrite"
 
 $contentToWrite | Set-Content -Path $FilePath -Force
 
