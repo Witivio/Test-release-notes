2da841f66ef50e647279df8c223b9c3a7bfd34f7
Gabrielle <gmarsallon@witivio.com>
2024-01-23 16:03:09 +0100

    try with objects

--- scripts/update_documentation_script_git.ps1
+++ scripts/update_documentation_script_git.ps1
@@ -2,14 +2,17 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$FilePath,
     [Parameter(Mandatory=$true)]
-    [string]$MarkdownData
+    [object]$JsonData
 )
+
+$choices = $JsonData.data.choices
+$markdownData = $choices[0].text
 
 $title = "## What's new on GPTPro ?"
 
-Write-Output "Debug :  $MarkdownData"
+Write-Output "Debug :  $markdownData"
 
-$contentToWrite = "$title`r`n$MarkdownData"
+$contentToWrite = "$title`r`n$markdownData"
 
 Write-Output "Debug2 :  $contentToWrite"
 
